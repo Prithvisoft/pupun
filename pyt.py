cf9eff33756c9c7e2da639a7b02fb5030f29bcbc
Prithvisoft <prithvipupun@gmail.com>
2022-01-11 02:18:32 -0500

    anything

---
+++ pyt.py
@@ -0,0 +1,24 @@
+// C program to print
+// ASCII Value of Character
+#include <stdio.h>
+int main()
+{
+	char c = 'k';
+
+	// %d displays the integer value of a character
+	// %c displays the actual character
+	printf("The ASCII value of %c is %d", c, c);
+	return 0;
+}
+// C program to print
+// ASCII Value of Character
+#include <stdio.h>
+int main()
+{
+	char c = 'k';
+
+	// %d displays the integer value of a character
+	// %c displays the actual character
+	printf("The ASCII value of %c is %d", c, c);
+	return 0;
+}
